9b9e230c131b84747ef11f5132b89de7165fa5bf
marvelnano <marvelnano@gmail.com>
2022-09-23 22:48:01 -0500

    Cambiando Inicio

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -10,7 +10,7 @@ PÁGINA DE INICIO
           <div class="col-sm-6">
             
             <h1 class="m-0 text-dark">
-              Todas las ofertas de tus webs de reserva favoritas.<!--Conócenos-->
+              Alissa Bazar.<!--Conócenos-->
               <!--<small class="text-sm">Acerca de nosotros</small>-->
             </h1>
             
