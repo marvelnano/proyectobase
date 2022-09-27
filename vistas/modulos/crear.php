5915032d1d9e5ec446fb822fa19ec47fec3fa142
marvelnano <marvelnano@gmail.com>
2022-09-27 17:17:36 -0500

    mejoras en crear

--- vistas/modulos/crear.php
+++ vistas/modulos/crear.php
@@ -13,21 +13,29 @@
         <div class="row" style="border-radius: 8px; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; margin: 20px; padding: 20px;">
             <legend>Crear Módulo</legend>
             <label class="col col-3">Módulo</label>
-            <div class="col col-5">            
+            <div class="col col-4">            
                 <input class="modulo" type="text" style="width: 100%;" />
             </div>
-            <span class="col col-3">Ingrese nombre de módulo en minúsculas</span>  
-            <button class="btn-primary crearModulo">Grabar</button>          
+            <div class="col col-3">
+                <span>Ingrese nombre de módulo en minúsculas</span>
+            </div> 
+            <div class="col col-2">             
+                <button class="btn-primary crearModulo">Grabar</button>
+            </div>          
         </div>
 
         <div class="row" style="border-radius: 8px; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; margin: 20px; padding: 20px;">
             <legend>Crear Controlador</legend>
             <label class="col col-3">Controlador</label>
-            <div class="col col-5">            
+            <div class="col col-4">            
                 <input class="controlador" type="text" style="width: 100%;" />
             </div>
-            <span class="col col-3">Ingrese nombre de controlador sin espacios y la primera palabra en mayúscula</span>
-            <button class="btn-primary crearControlador">Grabar</button>
+            <div class="col col-3">            
+                <span>Ingrese nombre de controlador sin espacios y la primera palabra en mayúscula</span>
+            </div>  
+            <div class="col col-2">             
+                <button class="btn-primary crearControlador">Grabar</button>
+            </div>
         </div>
     <div>
 </body>
