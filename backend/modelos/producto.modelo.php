e02be384549e661dafde753a385f07645c0e7d44
marvelnano <marvelnano@gmail.com>
2022-10-21 16:38:42 -0500

    subiendo el modelo de producto

--- backend/modelos/producto.modelo.php
+++ backend/modelos/producto.modelo.php
@@ -12,12 +12,30 @@
 			$conexion = new Conexion();
 
 			if($item != null){
-				$stmt = $conexion->conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+				$stmt = $conexion->conectar()->prepare("p.idproducto, n.idnegocio, n.razon_social, sc.idsubcategoria, sc.descripcion subcategoria,
+					cat.idcategoria, cat.descripcion categoria, c.idconsumidor, c.descripcion consumidor, m.idmedida, m.descripcion medida,
+					p.titulo, p.descripcion, p.codigo_sku, p.costo, p.precio, p.stock, p.imagen, p.estado 
+					FROM $tabla p
+					INNER JOIN subcategoria sc on sc.idsubcategoria = p.idsubcategoria 
+					INNER JOIN categoria cat on cat.idcategoria = sc.idcategoria
+					INNER JOIN consumidor c on c.idconsumidor = p.idconsumidor
+					INNER join negocio n on n.idnegocio = p.idnegocio
+					INNER JOIN medida m on m.idmedida = p.idmedida 
+					WHERE $item = :$item");
 				$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
 				$stmt -> execute();
 				return $stmt -> fetch();
 			}else{
-				$stmt = $conexion->conectar()->prepare("SELECT * FROM $tabla ORDER BY idproducto");
+				$stmt = $conexion->conectar()->prepare("p.idproducto, n.idnegocio, n.razon_social, sc.idsubcategoria, sc.descripcion subcategoria,
+					cat.idcategoria, cat.descripcion categoria, c.idconsumidor, c.descripcion consumidor, m.idmedida, m.descripcion medida,
+					p.titulo, p.descripcion, p.codigo_sku, p.costo, p.precio, p.stock, p.imagen, p.estado 
+					FROM $tabla p
+					INNER JOIN subcategoria sc on sc.idsubcategoria = p.idsubcategoria 
+					INNER JOIN categoria cat on cat.idcategoria = sc.idcategoria
+					INNER JOIN consumidor c on c.idconsumidor = p.idconsumidor
+					INNER join negocio n on n.idnegocio = p.idnegocio
+					INNER JOIN medida m on m.idmedida = p.idmedida 
+					ORDER BY sc.idsubcategoria");
 				$stmt -> execute();
 				return $stmt -> fetchAll();
 			}
@@ -32,10 +50,22 @@
 		static public function mdlIngresarProducto($tabla, $datos){
 			$conexion = new Conexion();
 
-			$stmt = $conexion->conectar()->prepare("INSERT INTO $tabla(descripcion, estado, usuariocrea, fechacrea) 
-			VALUES (:descripcion, 1, '20220001', NOW())");
+			$stmt = $conexion->conectar()->prepare("INSERT INTO $tabla(idnegocio, idsubcategoria, idconsumidor, idmedida, titulo, descripcion, codigo_sku,
+				costo, precio, stock, imagen, estado, usuariocrea, fechacrea) 
+				VALUES (:idnegocio, :idsubcategoria, :idconsumidor, :idmedida, :titulo, :descripcion, :codigo_sku, :costo, :precio, :stock, :imagen, 
+				1, '20220001', NOW())");
 
-			$stmt->bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR);    
+			$stmt->bindParam(":idnegocio", $datos["idnegocio"], PDO::PARAM_STR);    
+			$stmt->bindParam(":idsubcategoria", $datos["idsubcategoria"], PDO::PARAM_STR); 
+			$stmt->bindParam(":idconsumidor", $datos["idconsumidor"], PDO::PARAM_STR); 
+			$stmt->bindParam(":idmedida", $datos["idmedida"], PDO::PARAM_STR); 
+			$stmt->bindParam(":titulo", $datos["titulo"], PDO::PARAM_STR); 
+			$stmt->bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR); 
+			$stmt->bindParam(":codigo_sku", $datos["codigo_sku"], PDO::PARAM_STR); 
+			$stmt->bindParam(":costo", $datos["costo"], PDO::PARAM_STR); 
+			$stmt->bindParam(":precio", $datos["precio"], PDO::PARAM_STR); 
+			$stmt->bindParam(":stock", $datos["stock"], PDO::PARAM_STR); 
+			$stmt->bindParam(":imagen", $datos["imagen"], PDO::PARAM_STR); 
 			if($stmt->execute()){
 				return "ok";
 			}else{
@@ -76,11 +106,23 @@
 			//echo "idproductoEd: ".$datos["idproducto"];
 			$conexion = new Conexion();
 
-			$stmt = $conexion->conectar()->prepare("UPDATE $tabla SET descripcion = :descripcion, usuariomodifica = '20220002', 
-				fechamodifica = NOW()   
-			WHERE idproducto  = :idproducto");		
+			$stmt = $conexion->conectar()->prepare("UPDATE $tabla SET idnegocio = :idnegocio, idsubcategoria = :idsubcategoria, 
+				idconsumidor = :idconsumidor, idmedida = :idmedida, titulo = :titulo, descripcion = :descripcion, 
+				codigo_sku = :codigo_sku, costo = :costo, precio = :precio, stock = :stock, imagen = :imagen,  
+				imagen = :imagen, usuariomodifica = '20220002', fechamodifica = NOW()   
+				WHERE idproducto  = :idproducto");		
 			
-			$stmt -> bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR);
+			$stmt -> bindParam(":idnegocio", $datos["idnegocio"], PDO::PARAM_STR);    
+			$stmt -> bindParam(":idsubcategoria", $datos["idsubcategoria"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":idconsumidor", $datos["idconsumidor"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":idmedida", $datos["idmedida"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":titulo", $datos["titulo"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":descripcion", $datos["descripcion"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":codigo_sku", $datos["codigo_sku"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":costo", $datos["costo"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":precio", $datos["precio"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":stock", $datos["stock"], PDO::PARAM_STR); 
+			$stmt -> bindParam(":imagen", $datos["imagen"], PDO::PARAM_STR);
 			$stmt -> bindParam(":idproducto", $datos["idproducto"], PDO::PARAM_INT);
 
 			if($stmt->execute()){
