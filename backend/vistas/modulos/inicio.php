c0583a4217b1e8ab216db2fd0f56a31e09132eeb
marvelnano <marvelnano@gmail.com>
2023-04-22 22:51:29 -0500

    mejoras en el front y en el ws

--- backend/vistas/modulos/inicio.php
+++ backend/vistas/modulos/inicio.php
@@ -36,7 +36,7 @@ PÁGINA DE INICIO
                 <h5 class="card-title" style="box-sizing: border-box; margin: 20px 0px 10px; font-weight: 700; line-height: 1.1; font-size: 28px; font-family: Helvetica, Arial, sans-serif; padding: 0px; letter-spacing: 1.05px;">NOSOTROS</h5>
 
                 <p class="card-text" style="text-align: justify; box-sizing: border-box; margin: 0px 0px 10px; padding: 0px; font-family: Helvetica, Arial, sans-serif; font-size: 14px; letter-spacing: 1.05px;">
-                  Somos una tienda virtual únicamente para usuarios autorizados cuyo objetivo es Satisfacer las necesidades de nuestros clientes, haciendo de su viaje momentos y experiencias inolvidables, cumpliendo con los estándares de calidad y seguridad por parte de un equipo con actitud de servicio.
+                  Somos una tienda virtual únicamente para usuarios autorizados cuyo objetivo es Satisfacer las necesidades de nuestros clientes, haciendo de su compra online una experiencia inolvidable, cumpliendo con los estándares de calidad y seguridad por parte de un equipo con actitud de servicio.
                 </p>
                 <!--<a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>-->
@@ -50,7 +50,7 @@ PÁGINA DE INICIO
                 <h5 class="card-title" style="box-sizing: border-box; margin: 20px 0px 10px; font-weight: 700; line-height: 1.1; font-size: 28px; font-family: Helvetica, Arial, sans-serif; padding: 0px; letter-spacing: 1.05px;">Mision & Visión</h5><!--card-title-->
 
                 <p class="card-text" style="text-align: justify; box-sizing: border-box; margin: 0px 0px 10px; padding: 0px; font-family: Helvetica, Arial, sans-serif; font-size: 14px; letter-spacing: 1.05px;">
-                  Somos una empresa líder en el mercado audiovisual y marketing digital que está comprometida y enfocada en ayudar a marcas, eCommerces, retailers o empresas de todo tipo a exprimir el poder de lo visual. Conseguir que vendan más (y mejor), gracias a las mejores fotografías, vídeos, proyectos y demás contenido visual que sea necesario para conectar con sus clientes.
+                  Somos una empresa líder en el mercado de ventas online que está comprometida y enfocada en ayudar a marcas, retailers o empresas de todo tipo a exprimir el poder de lo visual. Conseguir que vendan más (y mejor), gracias a las mejores fotografías y demás contenido visual que sea necesario para conectar con sus clientes.
                 </p>
                 <!--<a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>-->
--- frontendPHP/infoproducto.php
+++ frontendPHP/infoproducto.php
@@ -1,44 +1,49 @@
 <!-- Product Info section-->
-<section class="py-5">
-    <?php 
-        $idprod = decrypt($urlidprod);
-        //echo 'idprod: '.$idprod;
-        $detalleProd = ControladorProducto::productoDetalle($idprod);
-        $detalleProd = json_decode($detalleProd, true);
-        //echo $detalleProd;
+<section class="py-3">
+  <?php
+    $idprod = decrypt($urlidprod);
+    //echo 'idprod: '.$idprod;
+    $detalleProd = ControladorProducto::productoDetalle($idprod);
+    $detalleProd = json_decode($detalleProd, true);
+    //echo $detalleProd;
 
-        if(empty($detalleProd['idproducto'])){
-            if(count($detalleProd) <= 0){
-                header("Location: ".appUrlFrontend."");
-            }
-        }
-        
-    ?>
-    <div class="container px-4 px-lg-5 my-5">
-        <div class="row gx-4 gx-lg-5 align-items-center">
-            <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="<?php echo appUrlBackend.$detalleProd['imagen'];?>" alt="..." /></div>
-            <div class="col-md-6">
-                <div class="small mb-1">SKU: <?php echo $detalleProd['codigo_sku'];?></div>
-                <h1 class="display-5 fw-bolder"><?php echo $detalleProd['titulo'];?></h1>
-                <div class="fs-5 mb-5">
-                    <?php
-                        if($detalleProd['precio_oferta'] != 0){
-                            echo '<span class="text-decoration-line-through">S/. ' . $detalleProd['precio_venta'] . '</span>';
-                            echo 'S/. ' . $detalleProd['precio_oferta'];
-                        }else{
-                            echo 'S/. ' . $detalleProd['precio_venta'];
-                        }
-                    ?>
-                </div>
-                <p class="lead"><?php echo $detalleProd['descripcion'];?></p>
-                <div class="d-flex">
-                    <input disabled class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
-                    <button class="btn btn-outline-dark flex-shrink-0" type="button" disabled>
-                        <i class="bi-cart-fill me-1"></i>
-                        Agregar al carrito
-                    </button>
-                </div>
-            </div>
-        </div>
+    if (empty($detalleProd['idproducto'])) {
+      if (count($detalleProd) <= 0) {
+        header("Location: " . appUrlFrontend . "");
+      }
+    }
+  ?>
+  <div class="container px-4 px-lg-5 my-3">
+    <div>
+      <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item text-sm"><a href="inicio">Inicio</a></li>
+        <li class="breadcrumb-item text-sm active">Detalle</li>
+      </ol>
     </div>
+    <div class="row gx-4 gx-lg-5 align-items-center">
+      <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="<?php echo appUrlBackend . $detalleProd['imagen']; ?>" alt="..." /></div>
+      <div class="col-md-6">
+        <div class="small mb-1">SKU: <?php echo $detalleProd['codigo_sku']; ?></div>
+        <h1 class="display-5 fw-bolder"><?php echo $detalleProd['titulo']; ?></h1>
+        <div class="fs-5 mb-5">
+          <?php
+          if ($detalleProd['precio_oferta'] != 0) {
+            echo '<span class="text-decoration-line-through">S/. ' . $detalleProd['precio_venta'] . '</span>';
+            echo 'S/. ' . $detalleProd['precio_oferta'];
+          } else {
+            echo 'S/. ' . $detalleProd['precio_venta'];
+          }
+          ?>
+        </div>
+        <p class="lead"><?php echo $detalleProd['descripcion']; ?></p>
+        <div class="d-flex">
+          <input disabled class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
+          <button class="btn btn-outline-dark flex-shrink-0" type="button" disabled>
+            <i class="bi-cart-fill me-1"></i>
+            Agregar al carrito
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
